cb59fe35f24f560fca96d870b04ff9a331337bb1
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-09-29 23:02:46 +0300

    Change form to use form_for helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+   
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
