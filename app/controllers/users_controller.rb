8537c3870e5f36b5504cc1abddc0b961771444a7
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-09-29 19:50:15 +0300

    Nest and whitelist form params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
       redirect_to new_user_path
@@ -11,4 +12,11 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,:email,:password)
+  end
+
 end
